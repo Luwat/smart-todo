bc598bcb5dcc71e32a12a1834c6611c9a3719738
luwat <oluwatobioreniyi@gmail.com>
2024-07-24 14:21:29 +0100

    Remove react-query

--- src/pages/TodoLayout.tsx
+++ src/pages/TodoLayout.tsx
@@ -1,16 +1,11 @@
 import { Outlet } from "react-router-dom";
 import SideNav from "../components/SideNav";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-
-const queryClient = new QueryClient();
 
 const TodoLayout = () => {
   return (
     <>
       <SideNav />
-      <QueryClientProvider client={queryClient}>
-        <Outlet />
-      </QueryClientProvider>
+      <Outlet />
     </>
   );
 };

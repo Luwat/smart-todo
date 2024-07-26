61171b3427f1b8c00367202ad2fcee1e97402bf1
luwat <oluwatobioreniyi@gmail.com>
2024-07-26 23:00:54 +0100

    Style Form components

--- src/components/ui/AddForm.tsx
+++ src/components/ui/AddForm.tsx
@@ -1,20 +1,41 @@
 import { FC } from "react";
-import { Form } from "react-router-dom";
+import { Form, Link } from "react-router-dom";
 
 const AddForm: FC = () => {
   return (
-    <Form method="post">
-      <legend>Enter Todo details.</legend>
-      <p>
-        <label htmlFor="title">Enter todo title:</label>
-        <input type="text" id="title" name="title"/>
+    <Form method="post" className="w-2/3 ml-10 mt-10 relative">
+      <legend className="text-2xl font-bold">Enter Todo details.</legend>
+      <p className="py-4">
+        <label htmlFor="title" className="block text-xl mb-2">
+          Enter todo title:
+        </label>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          className="rounded-md w-full"
+        />
       </p>
       <p>
-        <label htmlFor="description">Enter todo description:</label>
-        <textarea id="description" name="description"/>
+        <label htmlFor="description" className="block text-xl mb-2">
+          Enter todo description:
+        </label>
+        <textarea
+          id="description"
+          name="description"
+          className="rounded-md w-full"
+        />
       </p>
-      <div className="form-actions">
-        <button type="submit">Add Todo</button>
+      <div className="form-actions py-4 absolute right-0">
+        <Link to={".."}>
+          <button>Cancel</button>
+        </Link>
+        <button
+          type="submit"
+          className="rounded-sm border-2 py-2 px-4 ml-4 text-neutral-100 bg-neutral-700"
+        >
+          Add Todo
+        </button>
       </div>
     </Form>
   );
--- src/components/ui/EditForm.tsx
+++ src/components/ui/EditForm.tsx
@@ -3,35 +3,48 @@ import { Form, Link, useParams, useRouteLoaderData } from "react-router-dom";
 import type { Todos } from "../TodosList";
 
 const EditForm: FC = () => {
-  const params = useParams()
+  const params = useParams();
   const todos = useRouteLoaderData("todo") as Todos[];
-  
-  const todoIndex = todos.findIndex(todo => todo.id === params.id)
-  const todo = todos[todoIndex]
-  console.log(params.id)
-  console.log(todo)
-  console.log(todoIndex)
+
+  const todoIndex = todos.findIndex((todo) => todo.id === params.id);
+  const todo = todos[todoIndex];
 
   return (
-    <Form method="patch">
-      <legend>Enter Todo details.</legend>
-      <p>
-        <label htmlFor="title">Enter todo title:</label>
-        <input type="text" id="title" name="title" defaultValue={todo && todo.title} />
+    <Form method="patch" className="w-2/3 ml-10 mt-10 relative">
+      <legend className="text-2xl font-bold">Edit Todo details.</legend>
+      <p className="py-4">
+        <label htmlFor="title" className="block text-xl mb-2">
+          Enter todo title:
+        </label>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          defaultValue={todo.title}
+          className="rounded-md w-full p-4"
+        />
       </p>
       <p>
-        <label htmlFor="description">Enter todo description:</label>
+        <label htmlFor="description" className="block text-xl mb-2">
+          Enter todo description:
+        </label>
         <textarea
           id="description"
           name="description"
-          defaultValue={todo && todo.description}
+          defaultValue={todo.description}
+          className="rounded-md w-full p-4"
         />
       </p>
-      <div className="form-actions">
+      <div className="form-actions py-4 absolute right-0">
         <Link to={".."}>
           <button>Cancel</button>
         </Link>
-        <button type="submit">Edit Todo</button>
+        <button
+          type="submit"
+          className="rounded-md border-2 py-2 px-4 ml-4 text-neutral-100 bg-neutral-700"
+        >
+          Edit Todo
+        </button>
       </div>
     </Form>
   );

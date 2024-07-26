31fc1a122f4cca21ae228db45a068c390e382c05
luwat <oluwatobioreniyi@gmail.com>
2024-07-26 14:50:28 +0100

    Style homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,27 @@
 import { Link } from "react-router-dom";
+import bgImg from "../assets/todo.jpg";
 
 export default function HomePage() {
-    return <><h1>HomePage</h1> <Link to={'/todos'}>View all Todos</Link></>
+  return (
+    <main
+      className=" w-screen h-screen bg-center bg-cover bg-no-repeat"
+      style={{ backgroundImage: `url(${bgImg})` }}
+    >
+      <div className="flex justify-center items-center flex-col w-screen h-screen text-neutral-100 bg-neutral-700 bg-opacity-60 text-center">
+        <h1 className="text-4xl md:text-8xl m-8  uppercase font-bold">
+          Smart Todo
+        </h1>
+        <nav>
+          <Link
+            to={"/todos"}
+            className="w-52 h-48"
+          >
+            <li className="border-2 px-4 py-2 sm:px-8 sm:py-4 sm:text-2xl hover:text-neutral-700 hover:bg-neutral-100 border-neutral-100 hover:border-neutral-700 rounded-md list-none opacity-100">
+              View all Todos
+            </li>
+          </Link>
+        </nav>
+      </div>
+    </main>
+  );
 }

23c9f4f946f6848f5c3adfa91d3c9b60f7c0f618
luwat <oluwatobioreniyi@gmail.com>
2024-07-26 23:57:42 +0100

    Style TodoItem and TodosList components

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -2,18 +2,18 @@ import { Form, Link } from "react-router-dom";
 
 const TodoItem = ({ id, title }: { id?: string; title?: string }) => {
   return (
-    <section className="flex gap-10 px-4 md:px-8 m-4 text-lg rounded-lg justify-center items-center h-28 bg-neutral-50 shadow-lg">
-      <div className="w-8/12 my-4 flex gap-4 justify-center items-center">
+    <section className="flex gap-10 px-4 md:px-8 my-4 w-full text-lg rounded-lg justify-center items-center h-28 bg-neutral-50 shadow-lg">
+      <div className="w-8/12 my-4 flex gap-4 items-center">
         <p>
-          <input type="checkbox" className="rounded-full" />
+          <input type="checkbox" className="rounded-full hover:border-blue-500 hover:border-4" />
         </p>
         <Link to={`/todos/${id}`} className="">
-          <h1>{title}</h1>
+          <h1 className="hover:text-blue-500 hover:underline">{title}</h1>
         </Link>
       </div>
       <div className="flex gap-3 justify-center items-center">
         <Link to={`/todos/${id}/edit`}>
-          <button className="py-2 px-4 rounded-md bg-neutral-700 hover:bg-neutral-100 text-neutral-100 hover:text-neutral-700">
+          <button className="py-2 px-4 rounded-md bg-neutral-700 hover:bg-neutral-200 text-neutral-100 hover:text-neutral-700">
             Edit
           </button>
         </Link>
@@ -28,7 +28,7 @@ const TodoItem = ({ id, title }: { id?: string; title?: string }) => {
         >
           <button
             type="submit"
-            className="py-2 px-4 rounded-md bg-red-700 hover:bg-red-100 text-red-100 hover:text-red-700"
+            className="py-2 px-4 rounded-md bg-red-700 hover:bg-red-200 text-red-100 hover:text-red-700"
           >
             Delete
           </button>
--- src/components/TodosList.tsx
+++ src/components/TodosList.tsx
@@ -15,8 +15,8 @@ const TodosList = () => {
 
 
 
-    return <section>
-        <ul>
+    return <section className="lg:w-8/12 mx-auto">
+        <ul className="mx-4 my-10 lg:my-20">
             {data.map(todo => (
                 <TodoItem key={todo.id} {...todo}/>
             ))}

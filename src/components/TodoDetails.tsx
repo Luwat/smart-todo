c1439a6efb9fe3076a33f73a84ba7826a82ef5e9
luwat <oluwatobioreniyi@gmail.com>
2024-07-27 02:08:28 +0100

    Style TodoDetails component

--- src/components/TodoDetails.tsx
+++ src/components/TodoDetails.tsx
@@ -7,12 +7,12 @@ const TodoDetails = ({
     
   }) => {
     return (
-      <section>
+      <section className="my-8 mx-auto md:mx-8 md:my-20 w-10/12">
         <div>
-          <h1>{title}</h1>
-          <div>
-              <h3>Description</h3>
-              <p>{description}</p>
+          <h1 className="text-4xl font-semibold mb-6">{title}</h1>
+          <div className="">
+              <h3 className="text-2xl mb-4">Description:</h3>
+              <p className="text-lg">{description}</p>
           </div>
         </div>
       </section>

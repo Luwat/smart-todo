0a6b86f22c977603040f93a60fe4ae84e0616143
luwat <oluwatobioreniyi@gmail.com>
2024-07-27 01:58:03 +0100

    Fix "todo could be undefined" typescript error on details page

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -2,15 +2,23 @@ import TodoDetails from "../components/TodoDetails";
 import { useRouteLoaderData, useParams } from "react-router-dom";
 import type { Todos } from "../components/TodosList";
 
-const TodoDetailsPage = () => {
-    const params = useParams()
-    const data = useRouteLoaderData('todo') as Todos[]
-    const todo = data?.find((todo) => todo.id === params.id)
-    
-    console.log(params.id)
-    console.log(data)
+/*
+Type assertions have saved me a lot of typescript error issues.
+I had to use assertion after several other trials have refused to 
+stop throwing "todo could be undefined error"
+*/
 
-    return <TodoDetails title={todo.title} description={todo.description}/>
-}
+type Todo = {
+  title: string;
+  description: string;
+};
+
+const TodoDetailsPage = () => {
+  const params = useParams();
+  const data = useRouteLoaderData("todo") as Todos[];
+  const todo = data.find((todo) => todo.id === params.id) as Todo;
+
+  return <TodoDetails title={todo.title} description={todo.description} />;
+};
 
 export default TodoDetailsPage;
